1b4058ca114f6cd435d6fb833263299bdf0572a5
Kottala deepthi <reddydeepuh@gmail.com>
2026-01-21 15:15:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openLogin() {
+  document.getElementById("loginModal").style.display = "block";
+}
+
+function openSignup() {
+  document.getElementById("signupModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("loginModal").style.display = "none";
+  document.getElementById("signupModal").style.display = "none";
+}
